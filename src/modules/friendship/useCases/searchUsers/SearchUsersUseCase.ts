72c4891957c1f3483a67e508b6b1a1e08babcb01
kauan777 <kauan7622@gmail.com>
2022-12-10 19:35:34 -0300

    changing the return

--- src/modules/friendship/useCases/searchUsers/SearchUsersUseCase.ts
+++ src/modules/friendship/useCases/searchUsers/SearchUsersUseCase.ts
@@ -3,8 +3,13 @@ import { SearchUsersDTO } from "../../dtos/SearchUsersDTO";
 
 export class SearchUsersUseCase {
   async execute({ name }: SearchUsersDTO) {
-    
     const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        imagePath: true,
+      },
       where: {
         name: {
           contains: name,

a13411904d5b8b25a2d20b75949abf962f831df8
kauan777 <kauan.silva@d2d.dev.br>
2023-02-10 08:50:13 -0300

    feat: adding cors

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,14 @@ import { routes } from "./routes";
 const app = express();
 
 app.use(cors({ origin: "*" }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 app.use(routes);
 

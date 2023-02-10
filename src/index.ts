4dfdb4d320b5cb73be385835024a4451f12f7cdd
kauan777 <kauan.silva@d2d.dev.br>
2023-02-10 09:51:48 -0300

    adding hml domain

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,20 @@ import { routes } from "./routes";
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors({ origin: "http://env-9067380.jelastic.saveincloud.net" }));
+
 app.use(express.json());
 app.use(routes);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://env-9067380.jelastic.saveincloud.net"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
